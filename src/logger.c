81c62570c6bede781f4791197abb05b12f26be82
StjepanPrakljacic <stjepanprakljacic@gmail.com>
2024-06-23 15:18:15 +0200

    Removing a non-descriptive comments.

--- src/logger.c
+++ src/logger.c
@@ -10,15 +10,13 @@ void _log_message(LogLevel level, const char *message) {
         ANSI_COLOR_RED "LOG_ERROR" ANSI_COLOR_RESET
         };
 
-    // Get current time
     time_t raw_time;
     struct tm *time_info;
-    char time_buffer[30]; // Increased size to accommodate date and time
+    char time_buffer[30];
 
     time(&raw_time);
     time_info = localtime(&raw_time);
 
-    // Format time as [YYYY-MM-DD HH:MM:SS]
     strftime(
         time_buffer,
         sizeof(time_buffer),
@@ -26,6 +24,5 @@ void _log_message(LogLevel level, const char *message) {
         time_info
         );
 
-    // Print log message with timestamp and colored log level
     printf("%s %s %s\n", time_buffer, level_strings[level], message);
 }
